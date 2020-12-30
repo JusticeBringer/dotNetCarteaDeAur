adcc1d33b8889ba03521a9a673a6de0da42cd1b7
justicebringer <gabithebigg@gmail.com>
2020-12-30 22:34:55 +0200

    Add validation for PubContact

--- dotNetCarteaDeAur/Models/PubContact.cs
+++ dotNetCarteaDeAur/Models/PubContact.cs
@@ -11,18 +11,23 @@ namespace dotNetCarteaDeAur.Models
     public class PubContact
     {
         [Key]
+        // No need for validation - it is not completed by user
         public int Pub_contact_id { get; set; }
 
         [Required]
+        [RegularExpression(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$", ErrorMessage = "This is not a valid email!")]
         public string Pub_email { get; set; }
 
         [Required]
+        [RegularExpression(@"^07(\d{8})$", ErrorMessage = "This is not a valid phone number!")]
         public string Pub_phone { get; set; }
 
         [Required]
+        [RegularExpression(@"^[#.0-9a-zA-Z\s,-]+$", ErrorMessage = "This is not a valid city!")]
         public string Pub_city { get; set; }
 
         [Required]
+        [RegularExpression(@"^[#.0-9a-zA-Z\s,-]+$", ErrorMessage = "This is not a valid street!")]
         public string Pub_street { get; set; }
 
         // one-to-one
