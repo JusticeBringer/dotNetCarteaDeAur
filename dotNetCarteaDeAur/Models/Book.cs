399e2f788bdf78b2f73c9b2f473b0820979ab7c7
justicebringer <gabithebigg@gmail.com>
2020-12-30 23:05:24 +0200

    Add book validation

--- dotNetCarteaDeAur/Models/Book.cs
+++ dotNetCarteaDeAur/Models/Book.cs
@@ -13,35 +13,50 @@ namespace dotNetCarteaDeAur.Models
     public class Book
     {
         [Key]
+        // No need for validation - it is not completed by user
         public int Book_id { get; set; }
 
         [Required, 
         MinLength(10, ErrorMessage = "Title cannot be less than 10"),
-        MaxLength(800, ErrorMessage = "Title cannot be more than 80")]
+        MaxLength(80, ErrorMessage = "Title cannot be more than 80")]
+        [RegularExpression(@"^[a-zA-Z][a-zA-Z]{9,79}$", ErrorMessage = "This is not a valid book title!")]
+        // Title has to be start with letters 
+        // and contains letters characters in 10 to 80 length
         public string Title { get; set; }
 
         [Required,
         MinLength(4, ErrorMessage = "Author cannot be less than 4"),
         MaxLength(80, ErrorMessage = "Author cannot be more than 80")]
+        [RegularExpression(@"^[a-zA-Z][a-zA-Z]{3,79}$", ErrorMessage = "This is not a valid book author name!")]
+        // Author has to be start with letters 
+        // and contains letters characters in 4 to 80 length
         public string Author { get; set; }
 
         [Required]
+        // No validation here - using DateTime.TryParseExact at registering
         public DateTime PublishDate { get; set; }
 
         [Required]
+        [RegularExpression(@"[0-9]*[-| ][0-9]*[-| ][0-9]*[-| ][0-9]*[-| ][0-9]*", ErrorMessage = "This is not a valid ISBN number!")]
         public string ISBN { get; set; }
 
         [Required]
+        [RegularExpression(@"^\d*\.?\d*$", ErrorMessage = "This is not a valid price")]
+        // starts with number, ends with number, accepts decimals
         public double Price { get; set; }
 
         [Required]
+        [RegularExpression(@"^\d\d*$", ErrorMessage = "This is not a book quantity")]
+        // starts with number, ends with number
         public int Quantity { get; set; }
 
         [Required]
+        // No validation here - validation at file uploading
         public string ImagePath { get; set; }
 
         // one to many
         [Column("Pub_id")]
+        // No need for validation - it is not completed by user
         public int Pub_id { get; set; }
         public virtual Publisher Publisher { get; set; }
 
