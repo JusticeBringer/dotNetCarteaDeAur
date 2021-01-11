52741cf4347631019ad990bd6ebcad8ba2e3e261
Gabriel <gabithebigg@gmail.com>
2021-01-11 15:10:27 +0200

    Add orders to nav bar (#8)

---
+++ dotNetCarteaDeAur/Controllers/OrderController.cs
@@ -0,0 +1,22 @@
+﻿using dotNetCarteaDeAur.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace dotNetCarteaDeAur.Controllers
+{
+    public class OrderController : Controller
+    {
+        private DbCtx db = new DbCtx();
+        // GET: Order
+        public ActionResult Index()
+        {
+            List<Order> orders = db.Orders.ToList();
+            ViewBag.Orders = orders;
+
+            return View();
+        }
+    }
+}
